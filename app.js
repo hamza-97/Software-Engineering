df8fdfc81b9ee8bcde979eb05b2b3a9361ca25ee
Hamza Humayun <hamzahumayun@Hamzas-MacBook-Air.local>
2019-04-16 18:29:25 +0500

    last changes

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/views/splashscreen.html'));
 });
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8011;
 server=app.listen(port, () =>{
   console.log(`Server started on port ${port}`);
 });
